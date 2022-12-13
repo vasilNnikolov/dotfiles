3e098ebeacf0eb9fcc113ccaf91406f5f1690f51
vasil <vaskonikolov2003@gmail.com>
2022-12-13 22:27:43 +0200

    remove auto-compile for md files

--- .config/nvim/plugin/markdown.lua
+++ .config/nvim/plugin/markdown.lua
@@ -1,6 +1,6 @@
 -- vim.cmd([[
 --     autocmd BufWritePost *.md silent! !pandoc -V lang=bg % -o %:r.pdf 
 -- ]])
-vim.cmd([[
-    autocmd BufWritePost *.tex silent! !pdflatex %
-]])
+-- vim.cmd([[
+--     autocmd BufWritePost *.tex silent! !pdflatex %
+-- ]])
