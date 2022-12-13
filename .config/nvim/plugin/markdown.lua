a4112659cfc9291159fa20eec69ea53887781abe
vasil <vaskonikolov2003@gmail.com>
2022-12-13 22:24:56 +0200

    sync changes

--- .config/nvim/plugin/markdown.lua
+++ .config/nvim/plugin/markdown.lua
@@ -1,4 +1,6 @@
+-- vim.cmd([[
+--     autocmd BufWritePost *.md silent! !pandoc -V lang=bg % -o %:r.pdf 
+-- ]])
 vim.cmd([[
-    autocmd BufWritePost *.md silent! !pandoc -V lang=bg % -o %:r.pdf 
+    autocmd BufWritePost *.tex silent! !pdflatex %
 ]])
-
