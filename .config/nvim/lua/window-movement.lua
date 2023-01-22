e43c84f358908b2cca8f991343919cf10df1a62d
vasil <vaskonikolov2003@gmail.com>
2023-01-22 20:13:18 +0200

    shortcut for :tabnext in vim is ff because of 34kb

--- .config/nvim/lua/window-movement.lua
+++ .config/nvim/lua/window-movement.lua
@@ -1,3 +1,3 @@
 vim.keymap.set('n', '<Leader>a', '<C-w>h')
 vim.keymap.set('n', '<Leader>l', '<C-w>l')
-vim.keymap.set('n', '<Leader><Tab>', ':tabnext<cr>')
+vim.keymap.set('n', 'ff', ':tabnext<cr>')
